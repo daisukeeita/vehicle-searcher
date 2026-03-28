9509f939e1b83eddf3751b685c176ba26125eb2e
javv <vinas.johnandrei@gmail.com>
2026-03-29 05:43:05 +0800

    tried opening the implemented server

--- src/main/java/com/javv/vehicle/Main.java
+++ src/main/java/com/javv/vehicle/Main.java
@@ -1,17 +1,22 @@
 package com.javv.vehicle;
 
-import tools.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
-/**
- * The primary container for the application's execution logic.
- */
+import com.javv.vehicle.controller.VehicleController;
+import com.sun.net.httpserver.HttpServer;
+
+/** The primary container for the application's execution logic. */
 public class Main {
-  /**
-   * The entry point of the application.
-   */
-  public static void main(final String[] args) {
-    ObjectMapper mapper = new ObjectMapper();
-    System.out.println("Hello There");
+  /** The entry point of the application. */
+  public static void main(final String[] args) throws IOException {
+    InetSocketAddress socketAddress = new InetSocketAddress(8080);
+    HttpServer server = HttpServer.create(socketAddress, 0);
 
+    server.createContext("/", new VehicleController());
+
+    server.setExecutor(null);
+    System.out.println("Server started on port 8080...");
+    server.start();
   }
 }
