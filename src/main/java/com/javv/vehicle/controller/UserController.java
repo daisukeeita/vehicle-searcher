f66387de62289236d7c5d590d7111fc15aa1f567
javv <vinas.johnandrei@gmail.com>
2026-04-28 21:33:16 +0800

    Controller for user creation

---
+++ src/main/java/com/javv/vehicle/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.javv.vehicle.controller;
+
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class UserController implements HttpHandler {
+
+  @Override
+  public void handle(HttpExchange exchange) throws IOException {}
+
+  private void handlePost(HttpExchange exchange) throws IOException {}
+
+  private void handleGet(HttpExchange exchange) throws IOException {}
+}
