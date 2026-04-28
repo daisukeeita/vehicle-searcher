39533ccf0c6464ba580c2bc49f1da3b79eba16d1
javv <vinas.johnandrei@gmail.com>
2026-04-28 21:33:23 +0800

    class entity for users

---
+++ src/main/java/com/javv/vehicle/domain/user/User.java
@@ -0,0 +1,43 @@
+package com.javv.vehicle.domain.user;
+
+
+public class User {
+  private String id;
+  private String name;
+  private String username;
+  private String hashedPassword;
+
+  public User() {}
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setHashedPassword(String hashedPassword) {
+    this.hashedPassword = hashedPassword;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getHashedPassword() {
+    return hashedPassword;
+  }
+}
