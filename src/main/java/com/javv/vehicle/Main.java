d60ef891bb3643c4ffe860e99d9c012ed0af0a30
javv <vinas.johnandrei@gmail.com>
2026-04-28 21:33:58 +0800

    init

--- src/main/java/com/javv/vehicle/Main.java
+++ src/main/java/com/javv/vehicle/Main.java
@@ -2,17 +2,20 @@ package com.javv.vehicle;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.sql.SQLException;
 
 import com.javv.vehicle.controller.AuthController;
 import com.javv.vehicle.controller.VehicleController;
+import com.javv.vehicle.service.DatabaseService;
 import com.sun.net.httpserver.HttpServer;
 
 /** The primary container for the application's execution logic. */
 public class Main {
   /** The entry point of the application. */
-  public static void main(final String[] args) throws IOException {
+  public static void main(final String[] args) throws IOException, SQLException {
     InetSocketAddress socketAddress = new InetSocketAddress(8080);
     HttpServer server = HttpServer.create(socketAddress, 0);
+    DatabaseService databaseService = new DatabaseService();
 
     server.createContext("/", new VehicleController());
     server.createContext("/auth", new AuthController());
@@ -20,5 +23,7 @@ public class Main {
     server.setExecutor(null);
     System.out.println("Server started on port 8080...");
     server.start();
+
+    databaseService.postgresConnector();
   }
 }
